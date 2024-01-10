3e5ea5ddadc09b84982af138e90ac57f0d018b51
Seohyun Kang <143741901+seohyunKangSouthKorea@users.noreply.github.com>
2024-01-10 23:19:57 +0900

    [Silver V] Title: 영화감독 숌, Time: 60 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n;
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> n;
+	int i=666;
+	for(;;i++){
+		if(to_string(i).find("666")!=string::npos) n--;
+		if(n==0) break;
+	}
+	cout << i;
+}
