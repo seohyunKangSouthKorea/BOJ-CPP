96d8ef92b00d1f4cab31f24ead1725e7635e5756
Seohyun Kang <143741901+seohyunKangSouthKorea@users.noreply.github.com>
2024-01-10 23:52:58 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int a,b;
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    while(cin>>a>>b){
+        cout << a+b << '\n';
+    }
+}
