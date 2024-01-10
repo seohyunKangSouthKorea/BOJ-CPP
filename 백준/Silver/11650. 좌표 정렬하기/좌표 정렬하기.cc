209badfb9062fdaf0345cf1cff72e772d17a5952
Seohyun Kang <143741901+seohyunKangSouthKorea@users.noreply.github.com>
2024-01-10 23:54:55 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 120 ms, Memory: 3572 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n,x,y;
+vector<pair<int, int>> v;
+bool cmp(pair<int,int> a, pair<int,int> b){
+	if(a.first==b.first) return a.second < b.second;
+	return a.first < b.first;
+}
+int main()
+{
+	cin >> n;
+	for(int i=0;i<n;i++){
+		cin >> x >> y;
+		v.push_back({x,y});
+	}
+	sort(v.begin(),v.end(),cmp);
+	for(auto i:v)	cout << i.first << ' ' << i.second << '\n';
+}
