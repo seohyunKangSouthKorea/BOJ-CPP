f0c902795bb1774afbde670d50bfb175fdb8bbed
Seohyun Kang <143741901+seohyunKangSouthKorea@users.noreply.github.com>
2024-01-10 23:47:05 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+long long a,b,c;
+int main()
+{
+    cout << "|\\_/|\n";
+    cout << "|q p|   /}\n";
+    cout << "( 0 )\"\"\"\\\n";
+    cout << "|\"^\"`    |\n";
+    cout << "||_/=\\\\__|\n";
+}
