2e8349a9b23c0c38ad8502830d8b9ecbe1de6682
Seohyun Kang <143741901+seohyunKangSouthKorea@users.noreply.github.com>
2024-01-10 23:45:06 +0900

    [Bronze V] Title: 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n,sum;
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n;
+    for(int i=1;i<=n;i++)   sum +=i;
+    cout << sum;
+}
